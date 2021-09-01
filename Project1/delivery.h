81f04e7ae4a8ef542c61ee406143440d5e1ce632
miner90027 <alekszander.mccormick@outlook.com>
2021-08-31 20:58:26 -0800

    Built prototypes for the default constructor, constructor w/ parameters for the product, amount, and month created prototypes for setting the member variables created the member variables

--- Project1/delivery.h
+++ Project1/delivery.h
@@ -5,9 +5,38 @@
 #ifndef FALL2021_CS311_DELIVERY_H
 #define FALL2021_CS311_DELIVERY_H
 
+#include <string>
 
 class Delivery {
+    /*  Constructors */
+public:
+    Delivery(){
 
+    }
+
+    Delivery(std::string prdct, int qunt, int mon){
+
+    }
+
+    /*  Set data members */
+    void setProduct(std::string prdct){
+
+    }
+
+    void setQuantity(int qunt){
+
+    }
+
+    void setMonth(int mon){
+
+    }
+
+
+    /*  Data members */
+private:
+    std::string _product;
+    int _month;
+    int _quantity;
 };
 
 

62897fb200b358c9104389187dc0c3312410aef7
miner90027 <alekszander.mccormick@outlook.com>
2021-08-31 21:10:04 -0800

    Defined all the constructors defined the set functions built and defined the get functions

--- Project1/delivery.h
+++ Project1/delivery.h
@@ -5,32 +5,47 @@
 #ifndef FALL2021_CS311_DELIVERY_H
 #define FALL2021_CS311_DELIVERY_H
 
-#include <string>
+#include <string> // for std::string
+#include <cassert> // for assert
 
 class Delivery {
     /*  Constructors */
 public:
-    Delivery(){
+    Delivery(): Delivery("UNSPECIFIED", 0, 1)
+    {}
 
-    }
-
-    Delivery(std::string prdct, int qunt, int mon){
-
+    Delivery(std::string prdct, int qunt, int mon)
+    {
+        setProduct(prdct);
+        setQuantity(qunt);
+        setMonth(mon);
     }
 
     /*  Set data members */
     void setProduct(std::string prdct){
-
+        _product = prdct;
     }
 
     void setQuantity(int qunt){
-
+        _quantity = qunt;
     }
 
     void setMonth(int mon){
-
+        _month = mon;
     }
 
+    /*  Get data members */
+    std::string getProduct(){
+        return _product;
+    }
+
+    int getQuantity(){
+        return _quantity;
+    }
+
+    int getMonth(){
+        return _month;
+    }
 
     /*  Data members */
 private:

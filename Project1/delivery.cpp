fd35d297dc2b8fb0bc811cdb69086376807a8463
miner90027 <alekszander.mccormick@outlook.com>
2021-09-04 11:13:48 -0800

    Added assertions to guaranty that the functions will work properly

--- Project1/delivery.cpp
+++ Project1/delivery.cpp
@@ -20,6 +20,8 @@ Delivery::Delivery(): Delivery("UNSPECIFIED", 0, 1)
 // takes in the  product name, amount, and month of delivery
 Delivery::Delivery(std::string prdct, int qunt, int mon)
     {
+		assert(qunt >= 0);
+		assert(mon >= 1 && mon <= 12);
 		setName(prdct);
         setMonth(mon);
         setQuantity(qunt);
@@ -38,6 +40,7 @@ void Delivery::setName(std::string prdct){
 //setQuantity
 //sets the amount of product to the int passed so long as it is positive
 void Delivery::setQuantity(int qunt){
+		assert(qunt >= 0);
         _quantity = qunt;
     }
 
@@ -45,6 +48,7 @@ void Delivery::setQuantity(int qunt){
 //sets the month of delivery to the int passed so long as it is within
 // the specified range between 1 && 12
 void Delivery::setMonth(int mon){
+		assert(mon >= 1 && mon <= 12);
         _month = mon;
     }
 
@@ -61,12 +65,14 @@ std::string Delivery::getName() const &{
 //getQuantity
 //returns the amount of product as an int
 int Delivery::getQuantity() const &{
+		assert(_quantity >= 0);
         return _quantity;
     }
 
 //getMonth
 //returns the month of delivery as an int
 int Delivery::getMonth() const &{
+		assert(_month >= 1 && _month <= 12);
         return _month;
     }
 
@@ -78,14 +84,16 @@ int Delivery::getMonth() const &{
 //converts the data stored in a Delivery object into a readable string
 //formatted as such: _product (_quantity), _month of the year
 std::string Delivery::toString() const{
-    std::string output = getName() + " (" + std::to_string(getQuantity()) +"), " + _year[_month];
+	assert(getQuantity() >= 0);
+	assert(getMonth() >= 1 && getMonth() <= 12);
+    std::string output = getName() + " (" + std::to_string(getQuantity()) +"), " + _year[getMonth()];
     return output;
 }
 
 //empty
 // returns true if the quantity is zero and false if otherwise
 bool Delivery::empty() const & {
-
+	assert(getQuantity() >= 0);
     return (_quantity == 0);
 }
 
@@ -97,13 +105,16 @@ bool Delivery::empty() const & {
 //compares two Delivery objects and returns true if all the data members
 // are the same
 bool Delivery::operator==(const Delivery &d2) const{
+	assert(getQuantity() >= 0);
+	assert(getMonth() >= 1 && getMonth() <= 12);
     return (this->getMonth() == d2.getMonth() && this->getQuantity() == d2.getQuantity() && this->getName() == d2.getName());
 }
 
 //operator!=
 //compares two Delivery objects but returns the opposite of operator==
 bool Delivery::operator!=(const Delivery &d2) const{
-
+	assert(getQuantity() >= 0);
+	assert(getMonth() >= 1 && getMonth() <= 12);
     return !(*this == d2);
 }
 
@@ -114,6 +125,7 @@ bool Delivery::operator!=(const Delivery &d2) const{
 //pre-increment operator++
 //increments the quantity of a Delivery object by 1
 Delivery &Delivery::operator++() {
+	assert(getQuantity() >= 0);
 	this->setQuantity(this->getQuantity() +1);
 	return *this;
 }
@@ -121,6 +133,7 @@ Delivery &Delivery::operator++() {
 //pre-decrement operator--
 //decrements the quantity of a Delivery object by 1 if it is greater than 0
 Delivery &Delivery::operator--() {
+	assert(getQuantity() >= 0);
 	if(this->getQuantity() > 0)
 		this->setQuantity(this->getQuantity() -1);
 	return *this;
@@ -129,6 +142,7 @@ Delivery &Delivery::operator--() {
 //post-increment operator++
 //increments the quantity of a Delivery object by 1
 Delivery Delivery::operator++([[maybe_unused]]int dummy){
+	assert(getQuantity() >= 0);
 	auto save = *this;
 	++(*this);
 	return save;
@@ -137,6 +151,7 @@ Delivery Delivery::operator++([[maybe_unused]]int dummy){
 //post-decrement operator--
 //decrements the quantity of a Delivery object by 1 if it is greater than 0
 Delivery Delivery::operator--([[maybe_unused]]int dummy){
+	assert(getQuantity() >= 0);
 	auto save = *this;
 	--(*this);
 	return save;
@@ -149,6 +164,8 @@ Delivery Delivery::operator--([[maybe_unused]]int dummy){
 //operator<<
 // outputs the string created by toString into an output stream
 std::ostream &operator<<(std::ostream &os, const Delivery &dliv){
+	assert(dliv.getQuantity() >= 0);
+	assert(dliv.getMonth() >= 1 && dliv.getMonth() <= 12);
 	os << dliv.toString();
     return os;
 }

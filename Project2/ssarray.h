e1d52ea5264a67d8e11f1fd458dcec01f1ef92ef
miner90027 <alekszander.mccormick@outlook.com>
2021-09-10 11:14:40 -0800

    Initial creation of ssarray.h

---
+++ Project2/ssarray.h
@@ -0,0 +1,24 @@
+/*
+ * SSArray.h
+ * Aleks McCormick
+ * 2021/09/10
+ * Fall 2021 CS 311
+ * The header file for a somewhat smart array template class
+ */
+
+#ifndef FALL2021_CS311_SSARRAY_H
+#define FALL2021_CS311_SSARRAY_H
+
+#include <cassert> // for assert
+#include <ostream> // for std::ostream
+
+/* Class Invariants:
+ *
+ */
+
+template<typename Type>
+class SSArray{
+
+};
+
+#endif //FALL2021_CS311_SSARRAY_H

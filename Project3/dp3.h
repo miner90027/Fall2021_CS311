5b78fea972917c74028158adc580d79c7cb18889
miner90027 <alekszander.mccormick@outlook.com>
2021-09-17 15:37:59 -0800

    Updated boilerplate & other comments to match my coding style. Updated function formatting to match my programing style.

--- Project3/dp3.h
+++ Project3/dp3.h
@@ -1,12 +1,13 @@
-// dp3.h  SKELETON
-// Glenn G. Chappell
-// 2021-09-16
-//
-// For CS 311 Fall 2021
-// Header for Project 3 functions
+/*
+ * dp3.cpp
+ * Aleks McCormick
+ * 202/09/17
+ * Fall 2021 CS 311
+ * Source file for Project 3 functions
+ */
 
-#ifndef FILE_DP3_H_INCLUDED
-#define FILE_DP3_H_INCLUDED
+#ifndef FALL2021_CS311_DP3_H
+#define FALL2021_CS311_DP3_H
 
 #include "llnode.h"    // For LLNode
 #include <cstddef>     // For std::size_t
@@ -14,32 +15,26 @@
 
 
 template <typename ValueType>
-ValueType lookup(const LLNode<ValueType> * head,
-                 std::size_t index)
-{
+ValueType lookup(const LLNode<ValueType> * head, std::size_t index){
     return ValueType();  // Dummy return
     // TODO: WRITE THIS!!!
 }
 
 
 // Implementation in source file
-void didItThrow(const std::function<void()> & ff,
-                bool & threw);
+void didItThrow(const std::function<void()> & ff, bool & threw);
 
 
 template <typename RAIter>
-std::size_t uniqueCount(RAIter first,
-                        RAIter last)
-{
+std::size_t uniqueCount(RAIter first, RAIter last) {
     return std::size_t(42);  // Dummy return
     // TODO: WRITE THIS!!!
 }
 
 
 // Implementation in source file
-int gcd(int a,
-        int b);
+int gcd(int a, int b);
 
 
-#endif  //#ifndef FILE_DP3_H_INCLUDED
+#endif  //FALL2021_CS311_DP3_H
 

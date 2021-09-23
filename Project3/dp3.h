cd4e4542e3f3e942b40606b1432c027b71a65c08
miner90027 <alekszander.mccormick@outlook.com>
2021-09-22 21:37:38 -0800

    fixed lookup() function so that it doesn't throw unnecessarily, also fixed it so that it doesn't go out of range when iterating through to the index
    finished the uniqueCount() function to count the number of unique items in a range

--- Project3/dp3.h
+++ Project3/dp3.h
@@ -13,31 +13,34 @@
 #include <cstddef>     // For std::size_t
 #include <functional>  // For std::function
 #include <stdexcept>   // For std::out_of_range
-
+#include <algorithm>   // For std::unique && std::sort
+#include <iterator>    // For std::distance
 
 template <typename ValueType>
 ValueType lookup(const LLNode<ValueType> * head, std::size_t index){
 
-    if(head->_next == nullptr)
-        throw std::out_of_range("The given LLNode is empty.");
+	auto* temp = head->_next;
+
+    if(!head) {
+		throw std::out_of_range("The given LLNode is empty.");
+	}
 
     std::size_t size = 0;
-    auto* temp = head->_next;
 
-    while(temp != nullptr){
-        ++size;
-        temp = temp->_next;
-    }
+    while(temp) {
+		++size;
+		temp = temp->_next;
+	}
 
     if(index < 0 || index > size)
         throw std::out_of_range("The given index is not within the range of the LLNode.");
 
-
-    for(int i = 0; i <= index; ++i){
+    for(int i = 0; i < index; ++i){
         head = head->_next;
     }
 
-    return head->_data; // Dummy return
+	delete temp;
+    return head->_data;
 }
 
 
@@ -47,8 +50,12 @@ void didItThrow(const std::function<void()> & ff, bool & threw);
 
 template <typename RAIter>
 std::size_t uniqueCount(RAIter first, RAIter last) {
-    return std::size_t(42);  // Dummy return
-    // TODO: WRITE THIS!!!
+
+	std::sort(first, last);
+	auto uLast = std::unique(first, last);
+	std::size_t count = std::distance(first, uLast);
+
+    return count;
 }
 
 

21d6e3c49591e403fc2e69d8205929dd9cc15d1d
miner90027 <alekszander.mccormick@outlook.com>
2021-09-22 22:45:18 -0800

    Added comments describing the functions & process Cleaned up code where needed

--- Project3/dp3.cpp
+++ Project3/dp3.cpp
@@ -4,30 +4,44 @@
  * 202/09/17
  * Fall 2021 CS 311
  * Source file for Project 3 functions
+ * Modified from the Skeletal file created by Prof. Chappell
  */
 
 #include "dp3.h"       // For Project 3 prototypes & templates
 #include <functional>  // For std::function
-using std::function;
 
-
-void didItThrow(const function<void()> & ff, bool & threw) {
+// didItThrow()
+// Used to determine if a function passed to it trows an exception
+// Sets a passed bool variable accordingly
+// Re-throws the error that the passed function throws
+void didItThrow(const std::function<void()> & ff, bool & threw) {
 	try{
+		// set the bool to false
 		threw = false;
+
+		// run the function
 		ff();
 	}
+	// catch all errors
 	catch(...)
 	{
+		// set the bool to true
 		threw = true;
+
+		// re-throw the caught error
 		throw;
 	}
 }
 
-
+// gcd()
+// Calculates the greatest common divisor of 2 ints
+// Function is recursive
+// Does Not Throw
 int gcd(int a, int b) {
-    // bse case
+    // base case
     if (a == 0)
         return b;
+
     // check if a > b & run again swapping values
     if(a > b)
         return gcd(b,a);

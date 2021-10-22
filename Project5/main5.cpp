e34027d5716889a7a20db8da1cef3bf77c40b153
miner90027 <alekszander.mccormick@outlook.com>
2021-10-22 12:57:53 -0800

    Created Project5 directory, local CMakeLists.txt, & local troubleshooting program main5.cpp

---
+++ Project5/main5.cpp
@@ -0,0 +1,14 @@
+/*
+ * FILE NAME
+ * Aleks McCormick
+ * YYYY/MM/DD
+ * Fall 2021 CS 311
+ * SHORT DESCRIPTION
+ */
+
+#include <iostream>
+
+int main(){
+    std::cout << "Hello, world!" << std::endl;
+    return 0;
+}

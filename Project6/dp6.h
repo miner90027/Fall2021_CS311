bf8807c41a311719967b7c124736be5ed1f70bfc
miner90027 <alekszander.mccormick@outlook.com>
2021-11-04 19:22:53 -0800

    Created dp6.h to hold template functions and class for project Imported dp6_test.cpp and llnode2.h for testing purposes

---
+++ Project6/dp6.h
@@ -0,0 +1,36 @@
+/*
+ * dp6.h
+ * Aleks McCormick
+ * 2021/11/04
+ * Fall 2021 CS 311
+ * Header file containing template
+ *  functions for Project 6
+ */
+
+#ifndef FALL2021_CS311_DP6_H
+#define FALL2021_CS311_DP6_H
+
+#include <utility>  	//for std::unique_ptr & std::make_unique
+#include "llnode2.h"
+
+// Exercise A - reverseList()
+// Reverses the order of the given linked list
+// Preconditions:
+//  	...
+// 
+template<typename ValType>
+void reverseList(std::unique_ptr<LLNode2<ValType>> & head){
+
+};
+
+// Exercise B - LLMap
+// A list of associated data sets
+// Invariants:
+//  	...
+//
+template<typename KeyType, typename ValueType>
+class LLMap{
+
+};
+
+#endif //FALL2021_CS311_DP6_H

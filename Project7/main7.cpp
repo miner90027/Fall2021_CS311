a2b8899cb169aa926153350e7bd7c504e6b363f7
miner90027 <alekszander.mccormick@outlook.com>
2021-11-05 22:23:58 -0800

    Created sub dir for Project 7 & local testing file

---
+++ Project7/main7.cpp
@@ -0,0 +1,14 @@
+/*
+ * main7.cpp
+ * Aleks McCormick
+ * 2021/11/05
+ * Fall 2021 CS 311
+ * SHORT DESCRIPTION
+ */
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, world!" << std::endl;
+	return 0;
+}

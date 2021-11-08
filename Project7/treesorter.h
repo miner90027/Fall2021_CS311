7026a1eef409a3099403ef0ec4747d16988dca5f
miner90027 <alekszander.mccormick@outlook.com>
2021-11-08 13:21:32 -0900

    wrote dummy code for the treesort function, as provided in the skeleton file

--- Project7/treesorter.h
+++ Project7/treesorter.h
@@ -9,4 +9,36 @@
 #ifndef FALL2021_CS311_TREESORTER_H
 #define FALL2021_CS311_TREESORTER_H
 
+#include <iterator>
+// For std::iterator_traits
+#include <algorithm>
+// For std::stable_sort, std::move
+#include <vector>
+// For std::vector
+#include <iterator>
+// For std::distance
+
+
+// treesort
+// Sort a given range using Treesort.
+// Pre:
+//     ???
+// Requirements on Types:
+//     ???
+// Exception safety guarantee:
+//     ???
+template<typename FDIter>
+void treesort(FDIter first, FDIter last)
+{
+	// Value is the type that FDIter points to
+	using Value = typename std::iterator_traits<FDIter>::value_type;
+
+	// THE FOLLOWING IS DUMMY CODE. IT WILL PASS ALL TESTS, BUT IT DOES
+	// NOT MEET THE REQUIREMENTS OF THE PROJECT.
+	std::vector<Value> buff(std::distance(first, last));
+	std::move(first, last, buff.begin());
+	std::stable_sort(buff.begin(), buff.end());
+	std::move(buff.begin(), buff.end(), first);
+}
+
 #endif //FALL2021_CS311_TREESORTER_H

57e48644554b3b9f0c83fb045b803b676947d4a4
miner90027 <alekszander.mccormick@outlook.com>
2021-11-15 14:15:18 -0900

    Created a template class called node for building the binary tree. holds the data, a shared pointer to the parent and the child nodes

--- Project7/treesorter.h
+++ Project7/treesorter.h
@@ -9,14 +9,11 @@
 #ifndef FALL2021_CS311_TREESORTER_H
 #define FALL2021_CS311_TREESORTER_H
 
-#include <iterator>
-// For std::iterator_traits
-#include <algorithm>
-// For std::stable_sort, std::move
-#include <vector>
-// For std::vector
-#include <iterator>
-// For std::distance
+#include <iterator> // For std::iterator_traits
+#include <algorithm> // For std::stable_sort, std::move
+#include <vector> // For std::vector
+#include <iterator> // For std::distance
+#include <memory> // For std::unique_ptr & std:: shared_ptr
 
 
 // treesort
@@ -40,5 +37,19 @@ void treesort(FDIter first, FDIter last)
 	std::stable_sort(buff.begin(), buff.end());
 	std::move(buff.begin(), buff.end(), first);
 }
+
+template<typename DataType>
+class Node{
+		using DataPtrS = std::shared_ptr<Node<DataType>>;
+	public:
+		explicit Node(DataType value, DataPtrS par = nullptr):_data(value),_parent( par ? par : nullptr),
+																_leftChild(nullptr), _rightChild(nullptr){}
+
+	private:
+		DataType _data;
+		DataPtrS _parent;
+		DataPtrS _leftChild;
+		DataPtrS _rightChild;
+	};
 
 #endif //FALL2021_CS311_TREESORTER_H

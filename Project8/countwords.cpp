181a1d897671d9f60e80780920f1e5cd5fe24ecb
miner90027 <alekszander.mccormick@outlook.com>
2021-11-22 13:46:48 -0900

    Wrote countwords.cpp to receive input from the user using getline() program currnetly just echos the input back to the user

--- Project8/countwords.cpp
+++ Project8/countwords.cpp
@@ -8,12 +8,23 @@
 
 #include <iostream>
 #include <fstream>
+#include <string>
 #include <map>
 
 using std::cout;
+using std::cin;
 using std::endl;
+using std::getline;
+using std::string;
 
 
 int main(){
+
+	string fileName;
+
+	cout << "Please enter a file path." << endl;
+	getline(cin, fileName);
+
+	cout << "Input: " << fileName << endl;
 	return 0;
 }

4b2eaf83d937a77e7022b15a2ffa1487b9688f7a
miner90027 <alekszander.mccormick@outlook.com>
2021-11-29 18:54:00 -0900

    Cleaned up code & added comments.

--- Project8/countwords.cpp
+++ Project8/countwords.cpp
@@ -3,59 +3,62 @@
  * Aleks McCormick
  * 2021/11/22
  * Fall 2021 CS 311
- * SHORT DESCRIPTION
+ * Project 8 Exercise A
+ * Counts the number of words in a file
  */
 
-#include <iostream>
-#include <fstream>
-#include <string>
-#include <map>
+#include <iostream> 	// For std::cout , std::cin , std::endl;
+#include <fstream>  	// For std::ifstream
+#include <string>   	// For std::string , std::getline
+#include <map>  		// For std::map
 
+// using statements for the library functions used for printing
+//  	I am too lazy to write out "std::" before every cout or endl
 using std::cout;
 using std::cin;
 using std::endl;
-using std::getline;
-using std::string;
-using std::ifstream;
 
 
 int main(){
 
 	// String to store the user input file name
-	string fileName;
-	ifstream fileIn(fileName);
+	std::string fileName;
+	std::ifstream fileIn(fileName);
 
-	// Receive the file path from the user
-	// Verify that the file can be found.
 	// Prompt the user for a file until a valid file is input
 	do{
+		// Receive the file path from the user
 		cout << "Please enter the file path for the file you wish to open.\nFile path:";
-		getline(cin, fileName);
+		std::getline(cin, fileName);
 		fileIn = std::ifstream (fileName);
 
+		// Verify that the file can be found.
 		if(!fileIn)
-			cout << "The input file could not be found. "
-					"Please specify a file to open, my need to specify the absolute file path." << endl;
+			cout << "The input file could not be found or opened. "
+					"Please specify a file to open, you my need to specify the absolute file path." << endl;
 	}while(!fileIn);
 
-	std::map<string, int> data;
-	string line;
+	// map to store the words in the file
+	std::map<std::string, int> data;
+	// Temp string to store each word wile iterating over the file
+	std::string word;
 
-	while(fileIn >> line){
-		++data[line];
+	// Get each word from the file and store it in the string
+	while(fileIn >> word){
+		// Store the string in the map & increment the associated int
+		++data[word];
 	}
-	/*
-	if(fileIn.eof()){
 
-	}*/
-
+	// Print out the total number of words by getting the size of the map
 	cout << "Number of distinct words: " << data.size() << endl << endl;
 
+	// Preform an in-order traversal of the map printing each Key-Value pair in
+	//  	lexicographic order
 	for(const auto & kvp: data){
 		cout << kvp.first << ": " << kvp.second << endl;
 	}
 
+	// Close the file
 	fileIn.close();
-	//cout << "Input: " << fileName << endl;
 	return 0;
 }
